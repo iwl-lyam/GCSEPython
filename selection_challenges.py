c162503b1859b9f9754729700311d5ac57fd5299
iwl-lyam <64089164+iwl-lyam@users.noreply.github.com>
2025-01-22 11:16:49 +0000

    Create selection_challenges.py

---
+++ selection_challenges.py
@@ -0,0 +1,29 @@
+'''
+num1 = int(input("number 1 "))
+num2 = int(input("number 2 "))
+
+if num1 > num2:
+    print(f"{num2} {num1}")
+elif num2 > num1:
+    print(f"{num1} {num2}")
+
+num = int(input("enter number "))
+if num >= 20:
+    print("too high")
+else:
+    print("thank you")
+
+num = int(input("enter number "))
+
+if num >= 10 and num <= 20:
+    print("thank you")
+else:
+    print("incorrect answer")
+
+colour = input("enter a colour ")
+if colour.lower() == "red":
+    print("I like red too")
+else:
+    print(f"I don't like {colour}, {colour} is for losers")
+
+'''
